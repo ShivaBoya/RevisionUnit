f7fe6373b606e4bdb767a527c79273b219f87b4c
ShivaBoya <shivasiddu80@gmail.com>
2025-08-30 13:50:25 +0530

    Updated

--- sprint1/Contest/MovieExplorer/src/utils/myFilter.js
+++ sprint1/Contest/MovieExplorer/src/utils/myFilter.js
@@ -1,11 +1,10 @@
-// Custom filter: DO NOT use Array.prototype.filter internally
 export function MyFilter(list, predicate) {
-const out = []
-for (let i = 0; i < list.length; i++) {
-const item = list[i]
-if (predicate(item, i, list)) {
-out[out.length] = item
-}
-}
-return out
+  const out = [];
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i];
+    if (predicate(item, i, list)) {
+      out[out.length] = item;
+    }
+  }
+  return out;
 }
